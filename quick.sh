3db380edf272fc2e9a00fb89ceb6603d5aadef28
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2019-03-29 14:14:42 +0000

    Modify buildroot image to take advantage of hardware random number generation

--- quick.sh
+++ quick.sh
@@ -1,11 +1,13 @@
 diff -urN -X obj.txt original/linux-4.20-rc2/drivers patched/linux-4.20-rc2/drivers | sed -e 's=original/==' -e 's=patched/==' >configs/0099-lowrisc-ethernet.patch
 rm -rf buildroot/output/build/linux-4.20-rc2
+make -C buildroot defconfig BR2_DEFCONFIG=../configs/buildroot_defconfig
 make -C buildroot
 cd build && ../riscv-pk/configure --host=riscv64-unknown-elf --with-payload=../buildroot/output/images/vmlinux --enable-logo --with-logo=../configs/logo.txt --enable-print-device-tree && make
 cd ..
 riscv64-unknown-elf-objcopy -S -O binary --change-addresses -0x80000000 build/bbl bbl.bin
 riscv64-unknown-elf-objdump -d -l -S build/bbl >bbl.dis
-riscv64-unknown-elf-objdump -d -l -S buildroot/output/images/vmlinux >vmlinux.dis
+rm -f vmlinux.dis
+#riscv64-unknown-elf-objdump -d -l -S buildroot/output/images/vmlinux >vmlinux.dis
 echo Uploading to genesys2.sm
 tftp genesys2.sm <<EOF
  bin
